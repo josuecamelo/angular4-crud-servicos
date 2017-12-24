0cb220f724445a22a9b01ac5a2deca51edbdd7d5
Josué Camelo <josueprg@gmail.com>
2017-12-24 10:38:36 -0200

    Criando Rotas
    Criando Rotas para Navegação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,13 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import { PostListComponent } from './post-list/post-list.component';
 
+const appRoutes: Routes = [
+    {path: '', pathMatch: 'full', component: PostListComponent},
+    {path: 'posts', component: PostListComponent},
+    {path: 'posts/create', component: PostSaveComponent},
+    {path: 'posts/:id/edit', component: PostSaveComponent},
+];
+
 @NgModule({
     declarations: [
         AppComponent,
