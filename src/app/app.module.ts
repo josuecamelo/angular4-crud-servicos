6b41b0bc76390172aa790c653918865d1373ec1e
Josué Camelo <josueprg@gmail.com>
2017-12-24 10:40:46 -0200

    Chamando Route OutLet
    Onde paginas serão exibidas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import { PostListComponent } from './post-list/post-list.component';
+import {Routes, RouterModule} from "@angular/router";
 
 const appRoutes: Routes = [
     {path: '', pathMatch: 'full', component: PostListComponent},
     {path: 'posts', component: PostListComponent},
-    {path: 'posts/create', component: PostSaveComponent},
-    {path: 'posts/:id/edit', component: PostSaveComponent},
+    //{path: 'posts/create', component: PostSaveComponent},
+    //{path: 'posts/:id/edit', component: PostSaveComponent},
 ];
 
 @NgModule({
@@ -18,6 +19,7 @@ const appRoutes: Routes = [
     ],
     imports: [
         BrowserModule,
+        RouterModule.forRoot(appRoutes),
     ],
     providers: [],
     bootstrap: [AppComponent]
