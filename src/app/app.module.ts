6f43127938ad1872680d9762806470e8c130331b
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2018-05-29 18:58:08 -0300

    Commit Inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,16 +9,18 @@ import {PostService} from "./services/post.service";
 import { PostSaveComponent } from './post-save/post-save.component';
 import { ButtonComponent } from './bootstrap/button/button.component';
 import { GlyphComponent } from './bootstrap/glyph/glyph.component';
-import {FormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {MessageService} from "./services/message.service";
 import { ModalComponent } from './bootstrap/modal/modal.component';
 import { AlertComponent } from './bootstrap/alert/alert.component';
+import { SistemasListComponent } from './sistemas-list/sistemas-list.component';
 
 const appRoutes: Routes = [
     {path: '', pathMatch: 'full', component: PostListComponent},
     {path: 'posts', component: PostListComponent},
     {path: 'posts/create', component: PostSaveComponent},
     {path: 'posts/:id/edit', component: PostSaveComponent},
+    {path: 'sistemas', component: SistemasListComponent},
 ];
 
 @NgModule({
@@ -30,12 +32,14 @@ const appRoutes: Routes = [
         GlyphComponent,
         ModalComponent,
         AlertComponent,
+        SistemasListComponent,
     ],
     imports: [
         BrowserModule,
         RouterModule.forRoot(appRoutes),
         HttpClientModule,
-        FormsModule
+        FormsModule,
+        ReactiveFormsModule
     ],
     providers: [PostService, MessageService],
     bootstrap: [AppComponent]
