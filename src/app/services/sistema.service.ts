e1b5957b52af270a61f2c4738d0ab0961f6c8656
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2018-05-30 07:48:36 -0300

    Paginação

--- src/app/services/sistema.service.ts
+++ src/app/services/sistema.service.ts
@@ -12,29 +12,7 @@ export class SistemaService{
     private url: string = 'http://localhost:8000/api/sistemas';
     constructor(private http: HttpClient, private api: ApiService) { }
 
-    /*getSistemas(): Promise<PaginationSistema>{
-        console.log('consultando');
-        return this.http.get(this.url)
-            .toPromise()
-            .then(response => response as PaginationSistema)
-            .catch(this.handleError);
-
-
-    }*/
-
     getSistemas(){
         return this.http.get<PaginationSistema>(this.url);
     }
-
-    /*getBookingsAtUrl(url: string): Promise<PaginatedBooking>{
-        return this.http.get(url)
-            .toPromise()
-            .then(response => response.json() as PaginatedBooking)
-            .catch(this.handleError);
-    }*/
-
-    /*private handleError(error: any): Promise<any> {
-        console.error('An error occurred', error); // for demo purposes only
-        return Promise.reject(error.message || error);
-    }*/
 }
